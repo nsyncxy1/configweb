a28e91ac7780b483fd4fd0456dc95d7466ed1e79
hwt <61077504@qq.com>
2021-10-13 15:54:02 +0800

    sql base64解码

--- read-file/src/main/java/com/dakai/readfile/dbclient/param/SqlDataParam.java
+++ read-file/src/main/java/com/dakai/readfile/dbclient/param/SqlDataParam.java
@@ -1,15 +1,16 @@
 package com.dakai.readfile.dbclient.param;
 
+import cn.hutool.core.codec.Base64;
 import com.dakai.readfile.dbclient.iter.IDbToken;
 import lombok.Data;
 import lombok.ToString;
+import org.springframework.util.Base64Utils;
 
 /**
  * 数据查询参数
  * @author yujie
  *
  */
-@Data
 @ToString
 public class SqlDataParam implements IDbToken {
 
@@ -21,4 +22,38 @@ public class SqlDataParam implements IDbToken {
 	//查询数据库id
 	private String dbId;
 
+	public String getSql() {
+
+		if (sql != null) {
+			String se1 = Base64.decodeStr(sql);
+			String se2 = se1.substring(1);
+			String sqlString = Base64.decodeStr(se2);
+			System.out.println(sqlString);
+			return sqlString;
+		}
+
+		return sql;
+	}
+
+	public void setSql(String sql) {
+		this.sql = sql;
+	}
+
+	@Override
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	@Override
+	public String getDbId() {
+		return dbId;
+	}
+
+	public void setDbId(String dbId) {
+		this.dbId = dbId;
+	}
 }
